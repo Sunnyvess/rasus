6c51d082d895200d5bc894b9baee935b35da3bd9
ndrljevic@gmail.com <ndrljevic@gmail.com@30716048-f30b-16a8-ca32-1c82fee2caa7>
2011-12-04 15:28:43 +0000

    v0.5
    Inicijalno uploadana verzija BEncodera

---
+++ Bencoder/BencoderTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FairTorrent;
+
+namespace BencoderTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Torrent torrent = new Torrent(@"D:\Downloads\BossTest.torrent");
+
+            Console.ReadLine();
+        }
+    }
+}
